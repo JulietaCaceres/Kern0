daf6203ffebb99f9bc8454dd8fc438fa075d0c25
Julieta Caceres <missPanda@localhost.localdomain>
2019-03-21 20:44:28 -0300

    Defining my own type of integers

---
+++ c99int.h
@@ -0,0 +1,24 @@
+//Renombro short de 2 bytes
+typedef short int16_t;
+
+//Renombro unsigned short de 2bytes
+typedef unsigned short uint16_t;
+
+//Renombro char de 1 byte
+typedef char int8_t;
+
+//Renombro unsigned char de 1 byte
+typedef unsigned char uint8_t;
+
+//Renombro int de 4 byte
+typedef int int32_t;
+
+//Renombro unsigned int de 4 byte
+typedef unsigned int uint32_t;
+
+//Renombro long  de 8 byte
+typedef long int64_t;
+
+//Renombro unsigned long  de 8 byte
+typedef unsigned long uint64_t;
+
