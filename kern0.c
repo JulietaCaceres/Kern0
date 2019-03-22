bb00481f93c7ee33a8e468f2894c9d41dc136474
Julieta Caceres <missPanda@localhost.localdomain>
2019-03-21 23:33:45 -0300

    Changing print Hola version 1

--- kern0.c
+++ kern0.c
@@ -1,9 +1,12 @@
-static volatile unsigned int* VGABUF = (volatile char*) 0xb8000;
+#include "c99int.h"
+static volatile uint64_t* VGABUF = (volatile char*) 0xb8000;
 
 void comienzo(void) {
 
-        volatile unsigned int *p = VGABUF;
-        *p =0x2F4B2F4F;
+        volatile uint64_t *p = VGABUF + 160;
+        *p =0xE04FE048;
+        p ++;
+        *p =0xE041E04C;
         while (1)
                 asm("hlt");
 }
