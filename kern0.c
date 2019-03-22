99720c80f0167b54bb5ce560b257f365368cb4bc
Julieta Caceres <missPanda@localhost.localdomain>
2019-03-21 23:36:04 -0300

    Adding print hello version 2

--- kern0.c
+++ kern0.c
@@ -1,9 +1,10 @@
-#include "c99int.h"
-static volatile uint64_t* VGABUF = (volatile char*) 0xb8000;
+ #include "c99int.h"
+    static volatile uint64_t* VGABUF = (volatile char*) 0xb8000;
 
 void comienzo(void) {
 
-        volatile uint64_t *p = VGABUF + 160;
+        volatile uint64_t *p = VGABUF;
+        p += 160;
         *p =0xE04FE048;
         p ++;
         *p =0xE041E04C;
